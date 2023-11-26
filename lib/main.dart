e9c4fabc93e1ab47ccad121ed12245fcf0018012
faysal9991 <faysaltanvir9991@gmail.com>
2023-11-26 21:03:55 +0600

    sign in & sign up ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:edtech_application/provider/theme_provider.dart';
-import 'package:edtech_application/screens/authentication/sigin_in_screen.dart';
+import 'package:edtech_application/screens/authentication/sigin_in.dart';
+import 'package:edtech_application/util/helper.dart';
 import 'package:edtech_application/util/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'di_container.dart' as di;
 void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(MultiProvider(
     providers: [
-      ChangeNotifierProvider(create: (context) => di.sl<ThemeProvider>()),],
+      ChangeNotifierProvider(create: (context) => di.sl<ThemeProvider>()),
+    ],
     child: const MyApp(),
   ));
 }
@@ -21,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey:Helper.navigatorKey,
       title: 'Flutter Demo',
       theme: Provider.of<ThemeProvider>(context,listen:false).darkTheme?AppTheme.getDarkModeTheme():AppTheme.getLightModeTheme(),
       home: const SignInScreen(),
@@ -28,4 +32,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-//AIzaSyCTfvhufxYWJLU_ks-ByaZQJ2eENyhhtHg
--- lib/util/theme/app_colors.dart
+++ lib/util/theme/app_colors.dart
@@ -5,24 +5,24 @@ const Color colorPrimaryDark = Color(0xFFF9C45A);
 const colorShadow = Color.fromRGBO(210, 210, 210, 0.23);
 const colorShadow2 = Color.fromRGBO(213, 213, 213, 0.25);
 
-class AppColors {
+
+class LightColors {
   //TODO: for Light Theme color
-  static const Color scaffold = Color(0xFF06113E);
-  static const Color primaryColorLight = Color(0xFF06113E);
+  static const Color scaffold = Color(0xFFEFF3F2);
+  static const Color primaryColorLight = Color(0xFF508D69);
+  static const Color secondColorLight = Color(0xFF9ADE7B);
   static const Color hintTextColorLight = Color(0xff9E9E9E);
-  static const Color imageBGColorLight = Color(0xffE2F0FF);
-  static const Color whiteColorLight = Color(0xFFFFFFFF);
-  static const Color unreadColorLight = Color(0xFF00ACFF);
-
+  static const Color unreadColorLight = Color(0xFFEEF296);
+  static const Color textColor = Colors.black;}
   //TODO: for Dark Theme Color
-  static const Color primaryColorDark = Color(0xFFF9C45A);
-  static const Color hintTextColorDark = Color(0xff9E9E9E);
-  static const Color imageBGColorDark = Color(0xffE2F0FF);
-  static const Color whiteColorDark = Color(0xFFFFFFFF);
-  static const Color unreadColorDark = Color(0xFF00ACFF);
-  static const Color feedback = Color(0xFF2365A8);
-  static const Color s = Color(0xFFF0F2F5);
 
 
+class DarkColours{
+  static const Color scaffold = Color(0xFFEFF3F2);
+  static const Color primaryColor = Color(0xFF508D69);
+  static const Color secondColor = Color(0xFF9ADE7B);
+  static const Color hintTextColor = Color(0xff9E9E9E);
+  static const Color unreadColor= Color(0xFFEEF296);
+  static const Color textColor = Colors.white;
 }
 
